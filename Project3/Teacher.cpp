064d91b73aad5ae3458ea709211db0e786686a0a
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2024-10-09 20:42:25 +0300

    Обьявил поля и методы

--- Project3/Teacher.cpp
+++ Project3/Teacher.cpp
@@ -1,1 +1,21 @@
 #include "Teacher.h"
+
+Teacher::Teacher()
+{
+}
+
+Teacher::Teacher(const char* N, int a, const char* S)
+{
+}
+
+Teacher::~Teacher()
+{
+}
+
+void Teacher::Input()
+{
+}
+
+void Teacher::Output()
+{
+}
--- Project3/Teacher.h
+++ Project3/Teacher.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "Human.h"
-class Teacher :
-    public Human
+class Teacher : public Human
 {
+	char* subject;
+public:
+	Teacher();
+	Teacher(const char* N, int a, const char* S);
+	~Teacher();
+	void Input();
+	void Output();
 };
 
