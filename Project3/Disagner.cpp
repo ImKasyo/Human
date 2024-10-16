c9b273c1b240306517e99b755aac9884a2ef25f7
Mandi_pv66 <Mandi_pv66@student.itstep.org>
2024-10-16 20:12:34 +0300

    \

--- Project3/Disagner.cpp
+++ Project3/Disagner.cpp
@@ -1,15 +1,25 @@
 #include "Disagner.h"
+#include<iostream>
+using namespace std;
 
 Disagner::Disagner()
 {
+	Position = nullptr;
+	Company = nullptr;
 }
 
 Disagner::Disagner(char* p, char* c)
 {
+	Position = new char[strlen(p) + 1];
+	strcpy_s(Position, strlen(p) + 1, p);
+
+	Company = new char[strlen(c) + 1];
+	strcpy_s(Company, strlen(c) + 1, c);
 }
 
 void Disagner::Output()
 {
+	cout << "Position: " << Position << ", Company: " << Company;
 }
 
 void Disagner::Salary()
@@ -18,6 +28,8 @@ void Disagner::Salary()
 
 Disagner::~Disagner()
 {
+	delete[] Position;
+	delete[] Company;
 }
 
 
